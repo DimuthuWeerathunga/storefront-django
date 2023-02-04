ec2e478f26d8808ecfda94ef510981fd35e78548
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-02-04 19:49:09 +0530

    cart add functionality

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -6,13 +6,17 @@ from pprint import pprint
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='product')
 router.register('collections', views.CollectionViewSet)
+router.register('carts', views.CartViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
-urlpatterns = router.urls + products_router.urls
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
-pprint(router.urls)
+urlpatterns = router.urls + products_router.urls + carts_router.urls
+
+# pprint(router.urls)
 # if you have specific urls other than the ones coming from the router u can
 # include the router urls and the rest like below
 
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -2,13 +2,15 @@ from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from store.filters import ProductFilter
-from store.models import Product, Collection, OrderItem, Review
+from store.models import Product, Collection, OrderItem, Review, Cart, CartItem
 from store.pagination import CustomPageNumberPagination
-from store.serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from store.serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, \
+    CartItemSerializer, AddCartItemSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -199,3 +201,19 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        return CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])\
+            .select_related('product')
