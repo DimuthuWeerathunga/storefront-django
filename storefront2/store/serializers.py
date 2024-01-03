a388ef8317c388808d545ad0b493d79bca6d520d
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2024-01-03 23:21:51 +0530

    new

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -83,6 +83,41 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 class AddCartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
+    quantity = serializers.IntegerField()
+
+    def validate_product_id(self, value):
+        try:
+            Product.objects.get(pk=value)
+        except Product.DoesNotExist:
+            raise serializers.ValidationError('No product with the id found')
+        return value
+
+    def validate_quantity(self, value):
+        product_id = self.initial_data['product_id']
+        print(product_id)
+        if not CartItem.objects.filter(product_id=product_id).exists():
+            if value < 1:
+                raise serializers.ValidationError("Cannot add items of negative quantity")
+            return value
+        elif CartItem.objects.get(product_id=product_id).quantity + value < 1:
+            raise serializers.ValidationError(
+                'Cannot reduce product quantity lower than 1. delete the product from the cart instead')
+        return value
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            # update an existing item
+            cart_item.quantity += quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            # create new cart item
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+        return self.instance
 
     class Meta:
         model = CartItem
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -214,6 +214,9 @@ class CartItemViewSet(ModelViewSet):
             return AddCartItemSerializer
         return CartItemSerializer
 
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])\
             .select_related('product')
